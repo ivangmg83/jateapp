c111614e457da1a269fba9a7a42d2f63b57b9d68
ivangmg <ivangmg@hotmail.com>
2023-03-26 18:36:31 -0700

    databse.js corrected

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,7 +14,7 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.error('putDb not implemented');
+  console.error('Posting on database...');
 
   const textsDb = await openDB('jate', 1);
 
@@ -25,7 +25,7 @@ export const putDb = async (content) => {
   const store = tx.objectStore('jate');
 
   // Use the .add() method on the store and pass in the content.
-  const request = store.add({ text: content });
+  const request = store.put({ id: 1, value: content });
 
   // Get confirmation of the request.
   const result = await request;
@@ -34,7 +34,7 @@ export const putDb = async (content) => {
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () =>{
-  console.error('getDb not implemented');
+  console.error('Getting from database...');
   
    // Create a connection to the database database and version we want to use.
    const textsDb = await openDB('jate', 1);
@@ -46,12 +46,12 @@ export const getDb = async () =>{
    const store = tx.objectStore('jate');
  
    // Use the .getAll() method to get all data in the database.
-   const request = store.getAll();
+   const request = store.get(1);
  
     // Get confirmation of the request.
    const result = await request;
    console.log('result.value', result);
-   return result;
+   return result?.value;
 
 } 
 
